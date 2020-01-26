58cba24efbe82eb1f9a8930897a8f91f9becaf28
gyuezhang <47130297+gyuezhang@users.noreply.github.com>
2020-01-26 17:24:32 +0800

    sort

--- GRSVR/Properties/AssemblyInfo.cs
+++ GRSVR/Properties/AssemblyInfo.cs
@@ -6,10 +6,10 @@ using System.Runtime.InteropServices;
 // 控制。更改这些特性值可修改
 // 与程序集关联的信息。
 [assembly: AssemblyTitle("GRSVR")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Server")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("GRSVR")]
+[assembly: AssemblyProduct("宝坻区地下水资源信息管理系统 服务端程序")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 //可以指定所有这些值，也可以使用“生成号”和“修订号”的默认值
 //通过使用 "*"，如下所示:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.1.0.0")]
-[assembly: AssemblyFileVersion("1.1.0.0")]
+[assembly: AssemblyVersion("1.2.0.0")]
+[assembly: AssemblyFileVersion("1.2.0.0")]
